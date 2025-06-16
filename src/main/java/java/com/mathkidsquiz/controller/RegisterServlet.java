b5f58486703eb5ca609f2e7515b2b957a55fa384
Naddyie <nadiahlatif09@gmail.com>
2025-06-16 19:45:05 +0800

    initial commit

--- src/main/java/java/com/mathkidsquiz/controller/RegisterServlet.java
+++ src/main/java/java/com/mathkidsquiz/controller/RegisterServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author User
  */
-@WebServlet("/register.jsp")
+@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
     
     private static final long serialVersionUID = 1L;
