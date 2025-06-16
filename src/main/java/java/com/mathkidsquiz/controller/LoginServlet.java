3ea2d6f5ff0f9d46915ca51d907b5d723ca8dcaa
Naddyie <nadiahlatif09@gmail.com>
2025-06-16 19:32:15 +0800

    initial commit

--- src/main/java/java/com/mathkidsquiz/controller/LoginServlet.java
+++ src/main/java/java/com/mathkidsquiz/controller/LoginServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpSession;
  *
  * @author User
  */
-@WebServlet("/index.jsp")
+@WebServlet("/index")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private UserDAO userDAO;
